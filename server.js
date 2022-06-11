5427fc0d301781b24c6b6ac9a203e19c1ad6b488
GhoZaa <ghoza16m@gmail.com>
2022-06-11 11:09:25 +0700

    tidy up lines of code

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
+
 let corsOptions = {
   origin: "http://localhost:8081",
 };
+
 app.use(cors(corsOptions));
+
 const initRoutes = require("./src/routes");
+
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
+
 const port = 8080;
 app.listen(port, () => {
   console.log(`Running at localhost:${port}`);
